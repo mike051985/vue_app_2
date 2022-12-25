a14d184baee2725260eee0cedbc43c3e230fa2c1
Michelet Saint-Preux <michelet2000@gmail.com>
2022-12-25 08:07:48 +0800

    components/HeaderDisplay.js

--- components/HeaderDisplay.js
+++ components/HeaderDisplay.js
@@ -1,16 +1,16 @@
 app.component('header-display', {
     props: {
         title: String,
-        showAddTask: Boolean
     },
+    
     template:
     /*html*/
     `<header>
         <h1>{{ title }}</h1>
         <button-display 
-            @btn-click="$emit('toggle-add-task')"
-            text= "Add Task"
-            color="green" 
+        @btn-click="$emit('toggle-add-task')"
+        text="Add Task"
+        color="green" 
         >       
         </button-display>
     </header>
